ad6c3c4dcff93fafd6590553b979baf8ea808084
jacobh3ll0 <jacob.rempel@ontariotechu.net>
2024-03-28 18:03:46 -0400

    updated frontend with knowledge from inclass activity

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -12,24 +12,36 @@ function newRoom(){
         .then(response => response.text())
         .then(response => enterRoom(response)); // enter the room with the code
 }
+
 function enterRoom(code){
 
-    // refresh the list of rooms
+    // refresh the list of rooms //TODO: jacob here: I assume we need to have an active list of rooms
 
     // create the web socket
     ws = new WebSocket("ws://localhost:8080/WSChatServer-1.0-SNAPSHOT/ws/"+code);
 
-
-    // parse messages received from the server and update the UI accordingly
     ws.onmessage = function (event) {
         console.log(event.data);
-        // parsing the server's message as json
         let message = JSON.parse(event.data);
+        document.getElementById("log").value += "[" + timestamp() + "] " + message.message + "\n";
+    }
 
-        // handle message
-
+    document.getElementById("input").addEventListener("keyup", function (event) {
+        if (event.key === "Enter") {
+            let request = {"type":"chat", "msg":event.target.value};
+            ws.send(JSON.stringify(request));
+            event.target.value = "";
         }
+    });
+
+
 }
 
+//credit to the class activity for the timestamp function
+function timestamp() {
+    let d = new Date(), minutes = d.getMinutes();
+    if (minutes < 10) minutes = '0' + minutes;
+    return d.getHours() + ':' + minutes;
+}
 
 
